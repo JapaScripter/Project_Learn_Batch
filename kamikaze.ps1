31225e9d2d53dd3ecceaca82848fefdd80ff2601
Yuri <80179611+JapaScripter@users.noreply.github.com>
2023-06-07 18:00:09 -0300

    Add files via upload

---
+++ kamikaze.ps1
@@ -0,0 +1,11 @@
+@echo off
+CLS
+:A
+set url="https://extobrasil142922.protheus.cloudtotvs.com.br:1604/00/#/login"
+start Chrome.exe %url%
+start Chrome.exe https://172.20.2.2:4443/
+start Chrome.exe https://mail.google.com/mail/
+start Chrome.exe https://docs.google.com/spreadsheets/d/1eDJdFEGILvFDy871awEbTJ7bygaCx7ULFxjaWv0CZvA/edit#gid=609009606
+start Chrome.exe --incognito https://web.whatsapp.com/%F0%9F%8C%90/pt_br
+start Chrome.exe --incognito https://mail.google.com/mail/
+start Chrome.exe --incognito https://replit.com/
---
+++ tokkotai.ps1
@@ -0,0 +1,5 @@
+@echo off
+CLS
+:A
+set url="https://172.20.2.2:4443/"
+start C:\FirefoxPortable32\Firefox.exe %url%
